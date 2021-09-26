6e5120442fb66a0d777c431b48b100c523bcb437
unknown <expertramim@gmail.com>
2021-09-27 00:18:07 +0600

    first commit

---
+++ src/data.js
@@ -0,0 +1,101 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Mac Headphone',
+            category:'headphone',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Apple',
+            rating:4.5,
+            numReviews:10,
+            descripition:'high quality product'
+        },
+
+        {
+            _id:'2',
+            name:'Mac Headphone',
+            category:'headphone',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Apple',
+            rating:4.5,
+            numReviews:10,
+            descripition:'high quality product'
+        },
+
+        {
+            _id:'3',
+            name:'Mac Headphone',
+            category:'headphone',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Apple',
+            rating:4.5,
+            numReviews:10,
+            descripition:'high quality product'
+        },
+
+        {
+            _id:'4',
+            name:'Mac Headphone',
+            category:'headphone',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Apple',
+            rating:4.5,
+            numReviews:10,
+            descripition:'high quality product'
+        },
+
+        {
+            _id:'5',
+            name:'Mac Headphone',
+            category:'headphone',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Apple',
+            rating:4.5,
+            numReviews:10,
+            descripition:'high quality product'
+        },
+
+        {
+            _id:'6',
+            name:'Mac Headphone',
+            category:'headphone',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Apple',
+            rating:4.5,
+            numReviews:10,
+            descripition:'high quality product'
+        },
+
+        {
+            _id:'7',
+            name:'Mac Headphone',
+            category:'headphone',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Apple',
+            rating:4.5,
+            numReviews:10,
+            descripition:'high quality product'
+        },
+
+        {
+            _id:'8',
+            name:'Mac Headphone',
+            category:'headphone',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Apple',
+            rating:4.5,
+            numReviews:10,
+            descripition:'high quality product'
+        },
+    ]
+}
+
+export default data;
